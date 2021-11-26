2379311132ce4674823df7afdcdac175760ad5ca
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-11-26 19:21:41 +0100

    Adding query

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const app = require("express")();
+const TerminusDBClient = require("@terminusdb/terminusdb-client")
+const team = process.env.TERMINUSDB_TEAM
+const key = process.env.TERMINUSDB_ACCESS_TOKEN
+const url = `https://cloud.terminusdb.com/${team}/`
+const client = new TerminusDBClient.WOQLClient(url,
+                    {user:"user@email.com", organization:team})
 
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-  res.send("Hello world");
+    const query = {
+        "type": "Polity",
+        "query" : { "@id" : "AfDurrn" }
+    }
+    result = await client.queryDocument(query, { as_list : true } );
+    res.send(JSON.stringify(result))
 });
 
 app.listen(PORT, () => {
-  console.log(`App up at port ${PORT}`);
+  console.log(`App up at port ${PORT}`)
 });
